0738bd35a2626f7930c66f5f7c74bc13d46a2874
jinliang <jinliang@huami.com>
2016-06-24 15:37:41 +0800

    Initial commit

---
+++ app/src/main/java/com/huadou/cn/MainActivity.java
@@ -0,0 +1,28 @@
+package com.huadou.cn;
+
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_main)
+    ConstraintLayout activityMain;
+    @BindView(R.id.btn_search)
+    Button btnSearch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+
+
+    }
+}
